76193baf6f2a0dd75137e697fa991b61ec7e12b2
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-22 22:20:03 +0300

    finish herp

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,12 @@
 import './hero.css';
+import heroImage from './../../assets/avataaars.svg';
 
 function Hero() {
     return <section className="hero">
-        <div className="hero-content flex flex-column gap-3 justify-content-center align-items-center">
+        <div className="hero-content d-flex flex-column gap-3 justify-content-center align-items-center">
+            <img src={heroImage} alt="Hero Image"/>
+            <h1 className="text-center text-white">Start Bootstrap</h1>
+            <p className="text-center text-white">Graphic Artist - Web Designer - Illustrator</p>
             
         </div>
     </section>
