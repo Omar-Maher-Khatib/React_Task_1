9a5a3bc2a9f65f64581980d17e8f54c2a8e7c701
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-22 22:44:12 +0300

    finish about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,5 +1,17 @@
 import './about.css';
 
 function About() {
+    return <section className="about">
+        <div className="about-content d-flex flex-column gap-3 justify-content-center align-items-center">
+            <h1 className="text-center text-white">About</h1>
+            <div className="about-text w-50 d-flex flex-row gap-3 justify-content-center align-items-center align-items-start">
+                <p className="text-start text-white w-50">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript 
+                    as well as optional SASS stylesheets for easy customization.</p>
+                <p className="text-start text-white w-50">You can create your own custom avatar for the masthead, 
+                    change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p>
+            </div>
+            
+        </div>
+    </section>
 }
 export default About;
