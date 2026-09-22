ce7ae50347e0df7c6f428558b86ab66b5e76266d
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-22 22:31:21 +0300

    finish portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,24 @@
 import './portfolio.css';
+import cabin from './../../assets/cabin.png';
+import cake from './../../assets/cake.png';
+import circus from './../../assets/circus.png';
+import game from './../../assets/game.png';
+import safe from './../../assets/safe.png';
+import submarine from './../../assets/submarine.png';
 
 function Portfolio() {
+    return <section className="portfolio">
+        <div className="container">
+            <h2 className="text-center">Portfolio</h2>
+            <div className="grid-images gap-5 d-flex flex-wrap justify-content-center align-items-center">
+                <img src={cabin} className="img-fluid"/>
+                <img src={cake} className="img-fluid"/>
+                <img src={circus} className="img-fluid"/>
+                <img src={game} className="img-fluid"/>
+                <img src={safe} className="img-fluid"/>
+                <img src={submarine} className="img-fluid"/>
+            </div>
+        </div>
+    </section>
 }
 export default Portfolio;
