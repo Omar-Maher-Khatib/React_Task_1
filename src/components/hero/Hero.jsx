ddb13e62ce43ad97e60d082b73cfef7d94c33a63
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-22 20:53:44 +0300

    Preparing files/2

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,12 @@
 import './hero.css';
+import Navbar from '../navbar/Navbar';
 
 function Hero() {
-    return <div>Hero</div>;
+    return (
+        <div>
+            <Navbar />
+            <div>Hero</div>
+        </div>
+    );
 }
 export default Hero;
