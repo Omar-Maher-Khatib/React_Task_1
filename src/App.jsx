0f0e685e0b80ea32d83cadf1f44ef928713323f6
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-23 10:27:33 +0300

    fix paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Hero from "./components/hero/Hero";
-import Portfolio from "./components/portfolio/Portfolio";
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Hero from "./components/hero/Hero.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
+import About from "./components/about/About.jsx";
+import Contact from "./components/contact/Contact.jsx";
 
 function App() {
   return <>
