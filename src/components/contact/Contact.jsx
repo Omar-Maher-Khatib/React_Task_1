9624faeebd3d5e036d23e94882f96fe87684aa49
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-23 09:55:19 +0300

    finish contact me

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,17 @@
 import './contact.css';
 
 function Contact() {
+    return <section className="contact container d-flex flex-column align-items-center">
+        <div className="contact-content d-flex flex-column align-items-center w-100">
+            <h2 className="contact-title mt-5">Contact me</h2>
+            <form className="contact-form d-flex flex-column align-items-start mt-5 w-50">
+                <input type="text" placeholder="Full name" className="form-control mb-3 border-0 border-bottom" />
+                <input type="email" placeholder="Email address" className="form-control mb-3 border-0 border-bottom" />
+                <input type="phone" placeholder="Phone number" className="form-control mb-3 border-0 border-bottom" />
+                <textarea placeholder="message" className="form-control border-0 border-bottom pb-5"></textarea>
+                <button type="submit" className="btn btn-primary">Send</button>
+            </form>
+        </div>  
+    </section>;
 }
 export default Contact;
