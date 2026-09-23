b2a7dd37b28900e963a3b7c3ff72bb5e845324f5
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-23 10:20:11 +0300

    footer finished / remaings -> icon images

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
     return <section className="contact container d-flex flex-column align-items-center">
         <div className="contact-content d-flex flex-column align-items-center w-100">
             <h2 className="contact-title mt-5">Contact me</h2>
-            <form className="contact-form d-flex flex-column align-items-start mt-5 w-50">
+            <form className="contact-form d-flex flex-column align-items-start mt-5 w-75">
                 <input type="text" placeholder="Full name" className="form-control mb-3 border-0 border-bottom" />
                 <input type="email" placeholder="Email address" className="form-control mb-3 border-0 border-bottom" />
                 <input type="phone" placeholder="Phone number" className="form-control mb-3 border-0 border-bottom" />
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import './footer.css';
 
 function Footer() {
     return <footer className="footer d-flex flex-column justify-content-center align-items-center">
-        <div className="footer-content container d-flex justify-content-around align-items-start pt-5">
+        <div className="footer-content container d-flex justify-content-between align-items-start pt-5">
             <div className="d-flex flex-column justify-content-center align-items-center">
                 <h2 className="text-center mb-4">Location</h2>
                 <p className="text-center">2215 John Daniel Drive <br />
@@ -13,6 +13,8 @@ function Footer() {
             </div>
             <div className="d-flex flex-column justify-content-center align-items-center">
                 <h2 className="text-center mb-4">About Freelancer</h2>
+                <p className="text-center">Freelance is a free to use, MIT licensed <br />
+                    Bootstrap theme created by <span className="bootstrap">Start Bootstrap</span>.</p>
             </div>
         </div>
         <div className="footer-bottom d-flex justify-content-center align-items-center pt-4 pb-2">
