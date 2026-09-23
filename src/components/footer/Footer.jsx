1e71d8f3beebf6cfbca860902fcbe90eb5a58935
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-23 10:15:17 +0300

    some adjustments on paddings

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,23 @@
 import './footer.css';
 
 function Footer() {
+    return <footer className="footer d-flex flex-column justify-content-center align-items-center">
+        <div className="footer-content container d-flex justify-content-around align-items-start pt-5">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <h2 className="text-center mb-4">Location</h2>
+                <p className="text-center">2215 John Daniel Drive <br />
+                Clark, MO 65243</p>
+            </div>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <h2 className="text-center mb-4">Around the Web</h2>
+            </div>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <h2 className="text-center mb-4">About Freelancer</h2>
+            </div>
+        </div>
+        <div className="footer-bottom d-flex justify-content-center align-items-center pt-4 pb-2">
+            <p className="text-center">Copyright &copy; Your Website 2023</p>
+        </div>
+    </footer>
 }
 export default Footer;
